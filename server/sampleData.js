ee44842f5b51e869ec0cbefe3b9680fc5dfa0f25
fulldev319 <orion@iMac-orion.local>
2023-01-08 17:14:29 -0500

    Project & Client queries

---
+++ server/sampleData.js
@@ -0,0 +1,48 @@
+// Projects
+const projects = [
+  {
+    id: "1",
+    clientId: "1",
+    name: "eCommerce Website",
+    description: "Lorem ipsum dolor sit amet,",
+    status: "In Progress",
+  },
+  {
+    id: "2",
+    clientId: "2",
+    name: "Dating App",
+    description: "App Lorem ipsum dolor sit amet,",
+    status: "In Progress",
+  },
+  {
+    id: "3",
+    clientId: "3",
+    name: "Shopping App",
+    description: "App Lorem ipsum dolor sit amet,",
+    status: "Done",
+  },
+];
+
+//Clients
+const clients = [
+  {
+    id: "1",
+    name: "Tony Stark",
+    email: "ironman@gmail.com",
+    phone: "343-567-4333",
+  },
+  {
+    id: "2",
+    name: "Natasha Romanova",
+    email: "blackwidow@gmail.com",
+    phone: "223-567-4333",
+  },
+  {
+    id: "3",
+    name: "John Oblien",
+    email: "hojndf@gmail.com",
+    phone: "784-567-4333",
+  },
+];
+
+module.exports = { projects, clients };
